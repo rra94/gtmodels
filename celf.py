12bc7c550486270dc69378db288d10f52315b65b
rragarwa <rishavraj.agarwal@gmail.com>
2019-04-29 19:25:27 -0400

    updated code

--- celf.py
+++ celf.py
@@ -1,4 +1,7 @@
 
+from random import uniform, seed
+import numpy as np
+import time
 import networkx as nx
 import ndlib.models.ModelConfig as mc
 import ndlib.models.epidemics.IndependentCascadesModel as ids
@@ -11,7 +14,38 @@ import itertools
 from operator import add
 import pandas as pd
 
+def IC(g,S,p=0.5,mc=1000):
+    """
+    Input:  graph object, set of seed nodes, propagation probability
+            and the number of Monte-Carlo simulations
+    Output: average number of nodes influenced by the seed nodes
+    """
+    
+    # Loop over the Monte-Carlo Simulations
+    spread = []
+    for i in range(mc):
+        
+        # Simulate propagation process      
+        new_active, A = S[:], S[:]
+        while new_active:
 
+            # For each newly active node, find its neighbors that become activated
+            new_ones = []
+            for node in new_active:
+                
+                # Determine those neighbors that become infected
+                np.random.seed(i)
+                success = {n: np.random.uniform(0,1)< p for n in g.neighbors(node)}
+                new_ones += [k for k in success if success[k]]
+
+            new_active = list(set(new_ones) - set(A))
+            
+            # Add newly activated nodes to the set of activated nodes
+            A += new_active
+            
+        spread.append(len(A))
+        
+    return(np.mean(spread))
 
 def celf(g,k,p=0.1,mc=1000):  
     """
@@ -25,14 +59,14 @@ def celf(g,k,p=0.1,mc=1000):
     
     # Calculate the first iteration sorted list
     start_time = time.time() 
-    marg_gain = [IC(g,[node],p,mc) for node in range(g.vcount())]
+    marg_gain = {node : IC(g,[node],p,mc) for node in g.nodes()}
 
     # Create the sorted list of nodes and their marginal gain 
-    Q = sorted(zip(range(g.vcount()),marg_gain), key=lambda x: x[1],reverse=True)
+    Q = sorted(marg_gain,key=marg_gain.get, reverse=True):,reverse=True)
 
     # Select the first node and remove from candidate list
     S, spread, SPREAD = [Q[0][0]], Q[0][1], [Q[0][1]]
-    Q, LOOKUPS, timelapse = Q[1:], [g.vcount()], [time.time()-start_time]
+    Q, LOOKUPS, timelapse = Q[1:], [g.number_of_nodes()], [time.time()-start_time]
     
     # --------------------
     # Find the next k-1 nodes using the list-sorting procedure
--- celf_wikivote.py
+++ celf_wikivote.py
@@ -1,4 +1,7 @@
-
+from random import uniform, seed
+import numpy as np
+import time
+import time 
 import networkx as nx
 import ndlib.models.ModelConfig as mc
 import ndlib.models.epidemics.IndependentCascadesModel as ids
@@ -7,7 +10,7 @@ import random
 from bokeh.io import output_notebook, show
 from ndlib.viz.bokeh.DiffusionTrend import DiffusionTrend
 import ndlib.models.epidemics.ThresholdModel as th
-form celf import celf
+from celf import celf
 import itertools
 from operator import add
 import pandas as pd
@@ -18,20 +21,22 @@ gg=[]
 
 
 with open("/home/rragarwa/Documents/nucleus/nd/wikivote_ps.txt", 'r') as f:
-    next(f)
-    for line in f:
-        k=line.split()
-        gg.append([k[0], k[1]])
+	next(f)
+	for line in f:
+		k=line.split()
+		gg.append([k[0], k[1]])
 
 
 g = nx.DiGraph()
 
 g.add_edges_from(gg)
 celf_output =[]
-for p in [0.0072,0.03,0.05, 0.1, 0.5, 1]:
-    for s in [1,5, 10,20, 35]:
-      celf_output.append([p, s] + celf(g,s, p=,mc=1000)[:2])
+#for p in [0.0072,0.03,0.05, 0.1, 0.5, 1]:
+for p in [0.0072, 0.03]:
+	for s in [1,5]:
+		celf_output.append([p, s] + list(celf(g,s,p,mc=2)))
+		print(celf_output)
 
 df=pd.DataFrame(celf_output)
 
-df.to_csv("/home/rragarwa/gt_models/wikivote_clef_1000.csv")      
+df.to_csv("/home/rragarwa/gt_models/wikivote_clef_1000.csv")
